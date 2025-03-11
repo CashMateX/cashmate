e32d76d7cafa95034b1ac4d79914f79d80b71d68
Damian Bergemann <damian.bergemann@outlook.com>
2025-03-11 16:38:32 +0100

    Enhance login form with state management and error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [showError, setShowError] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Image src="/banner-auth.png" alt="Logo" width={320} height={200} className="rounded-4xl" />
       <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
         <legend className="fieldset-legend">Login</legend>
+
+        {showError &&
+          <div role="alert" className="alert alert-error alert-soft">
+            <span>Error! {error}</span>
+          </div>
+        }
         
         <label className="fieldset-label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+        <input type="email" className="input" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
         
         <label className="fieldset-label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
+        <input type="password" className="input" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         
         <button className="btn btn-neutral mt-4">Login</button>
 
