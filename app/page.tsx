7fd241c7f98a954f03ad0c1555adb3700f1230b0
Damian Bergemann <damian.bergemann@outlook.com>
2025-03-11 16:47:38 +0100

    Add login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -10,6 +13,29 @@ export default function Login() {
 
   const [error, setError] = useState("");
   const [showError, setShowError] = useState(false);
+
+  const router = useRouter();
+
+  const login = async () => {
+    try {
+      const response = await axios.post("/api/auth/login", {
+        email: email,
+        password: password,
+      });
+
+      if (response.status === 200) {
+        Cookies.set("token", response.data.token);
+        router.push("/dashboard");
+      }
+    } catch (error: any) {
+      setError(error.response.data.error);
+      setShowError(true);
+
+      setTimeout(() => {
+        setShowError(false);
+      }, 5000);
+    }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -29,7 +55,7 @@ export default function Login() {
         <label className="fieldset-label">Password</label>
         <input type="password" className="input" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         
-        <button className="btn btn-neutral mt-4">Login</button>
+        <button className="btn btn-neutral mt-4" onClick={login}>Login</button>
 
         <div className="justify-center flex">
           <Link href="/register">Dont have an account? Register here</Link>
