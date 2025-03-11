0f3ed5ffcccd6bec89c645afbe8aadfc16e282ca
Damian Bergemann <damian.bergemann@outlook.com>
2025-03-11 21:54:48 +0100

    Title Fix

--- app/dashboard/assets/Assets.tsx
+++ app/dashboard/assets/Assets.tsx
@@ -5,8 +5,7 @@ import Layout from "@/components/Layout";
 export default function Dashboard() {
   return (
     <Layout>
-        
-      <p>Assets</p>
+        <h1 className="text-3xl font-bold mb-6">My Assets</h1>
     </Layout>
   );
 }
